910c0f05ea9d5e8426ae6bb9c4fdd7a7240e008c
filden <fildennn@gmail.com>
2025-11-28 23:42:00 +0700

    add naw

--- app/src/main/java/ru/filden/MainActivity.kt
+++ app/src/main/java/ru/filden/MainActivity.kt
@@ -4,80 +4,114 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
 import ru.filden.ui.theme.DutyScheduleTheme
 import ru.filden.logic.Schedule
 import ru.filden.logic.Student
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
     var students = arrayListOf<Student>(Student("loh", Random.nextLong(), 1),
-    Student("asd", Random.nextLong(), 2),
-    Student("qwe", Random.nextLong(), 3),
+        Student("asd", Random.nextLong(), 2),
+        Student("qwe", Random.nextLong(), 3),
         Student("123", Random.nextLong(), 4),
         Student("pdf", Random.nextLong(), 5),
-    Student("hfgfh", Random.nextLong(), 99))
+        Student("hfgfh", Random.nextLong(), 99))
     var schedule: Schedule = Schedule("123",students)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DutyScheduleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    viewDuty(schedule)
-                }
+        Main(schedule)
+        }
+    }
+}
+@Composable
+fun Main(schedule: Schedule){
+    val navController = rememberNavController()
+    Column {
+        navBar(navController)
+        NavHost(navController, startDestination = NavRoutes.mainPage.route){
+            composable ( NavRoutes.mainPage.route ){mainPage(schedule)}
+            composable ( NavRoutes.students.route ){students()}
+            composable ( NavRoutes.qr.route ){qr()}
+        }
+    }
+}
+@Composable
+fun navBar(navController: NavController){
+    Row(Modifier.padding(top=80.dp)){
+        Text("Дежурные", Modifier.clickable{navController.navigate(NavRoutes.mainPage.route)}.weight(0.28f), fontSize = 22.sp)
+        Text("Редактирование", Modifier.clickable{navController.navigate(NavRoutes.students.route)}.weight(0.52f), fontSize = 22.sp)
+        Text("QR", Modifier.clickable{navController.navigate(NavRoutes.qr.route)}.weight(0.20f), fontSize = 22.sp)
+    }
+}
+@Composable
+fun mainPage(schedule: Schedule){
+    val message = remember { mutableStateOf(schedule.firstDutyStudent) }
+
+    Surface(Modifier.fillMaxSize().padding(top=150.dp, bottom = 150.dp, start = 50.dp, end = 50.dp)) {
+        Column {
+            Text(
+                text = "Следующий дежурный: \n" + message.value.name, fontSize = 22.sp
+            )
+            Button(
+                onClick = { schedule.completeDutyStudent(schedule.firstDutyStudent,
+                    true)
+                    message.value=schedule.firstDutyStudent},
+                modifier = Modifier.padding(end = 10.dp)
+            ) {
+                Text("Продежурил", fontSize = 25.sp)
             }
+            Button(onClick = {
+                schedule.completeDutyStudent(
+                    schedule.firstDutyStudent,
+                    false)
+                message.value=schedule.firstDutyStudent
+            }) {
+                Text("Не продежурил", fontSize = 25.sp)
+            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun students(){
+
 }
 @Composable
-fun viewDuty(schedule: Schedule) {
-        Column(modifier = Modifier.padding(vertical = 50.dp)) {
-          DutyRow(schedule)
-            }
-        }
-@Composable
-fun DutyRow(schedule: Schedule) {
-    Row(modifier = Modifier.padding(5.dp).border(1.dp, color= Color.Gray)) {
-        var text = remember { mutableStateOf(schedule.firstDutyStudent.name)}
-        Text(
-            text = text.value
-        )
-        Button(onClick = { schedule.completeDutyStudent(schedule.firstDutyStudent, true)
-            text.value = schedule.firstDutyStudent.name
-        }) {
-            Text("Продежурил")
-        }
-        Button(onClick = {schedule.completeDutyStudent(schedule.firstDutyStudent, false)}) {
-            text.value = schedule.firstDutyStudent.name
-            Text("Не Продежурил")
-        }
-    }
+fun qr(){
+
+}
+sealed class NavRoutes(val route: String){
+    object mainPage: NavRoutes("mainPage")
+    object students: NavRoutes("students")
+    object qr: NavRoutes("qr")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.ir.backend.js.lower.collectNativeImplementations
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
